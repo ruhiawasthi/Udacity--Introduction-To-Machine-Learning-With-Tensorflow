99f2f69a8eb37edc84f7414a97e949d50a602571
Ruhi Awasthi <ruhiawasthi93rd@gmail.com>
2020-06-19 06:18:31 +0530

    I Am Creating An Unsorted Rank 1 Ndarray

---
+++ NumPy Codes/rank1 ndarray.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# We create an unsorted rank 1 ndarray
+import numpy as np
+x = np.random.randint(1,11,size=(10,))
+
+# We print x
+print()
+print('Original x = ', x)
+
+# We sort x and print the sorted array using sort as a function.
+print()
+print('Sorted x (out of place):', np.sort(x))
+
+# When we sort out of place the original array remains intact. To see this we print x again
+print()
+print('x after sorting:', x)
+
